55a3650a1f9fbc79b7e7f3d9348b8541df758f88
henrypadua <henrybpj@live.com>
2021-06-16 01:15:30 -0300

    configurar api stripe

--- src/components/SignInButton/index.tsx
+++ src/components/SignInButton/index.tsx
@@ -1,10 +1,19 @@
 import { FaGithub } from "react-icons/fa";
+import { FiX } from "react-icons/fi";
 import styles from "./styles.module.scss";
 
 export function SignInButton() {
-	return (
+	const isUserLoggedIn = true;
+
+	return isUserLoggedIn ? (
 		<button className={styles.singInButton} type="button">
-			<FaGithub />
+			<FaGithub color="#04d361" />
+			Sign in with Github
+			<FiX color="#737380" className={styles.closeIcon} />
+		</button>
+	) : (
+		<button className={styles.singInButton} type="button">
+			<FaGithub color="#eba417" />
 			Sign in with Github
 		</button>
 	);
